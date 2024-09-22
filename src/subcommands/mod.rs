b3e6fa0b00f98a9f62addb0f797a80b2f001d9e3
Moritz Hölting <87192362+moritz-hoelting@users.noreply.github.com>
2024-09-22 23:01:05 +0200

    implement basic migration subcommand

--- src/subcommands/mod.rs
+++ src/subcommands/mod.rs
@@ -7,12 +7,17 @@ pub use build::{build, BuildArgs};
 mod clean;
 pub use clean::{clean, CleanArgs};
 
-#[cfg(feature = "watch")]
-mod watch;
-#[cfg(feature = "watch")]
-pub use watch::{watch, WatchArgs};
-
 #[cfg(feature = "lang-debug")]
 mod lang_debug;
 #[cfg(feature = "lang-debug")]
 pub use lang_debug::{lang_debug, LangDebugArgs};
+
+#[cfg(feature = "migrate")]
+mod migrate;
+#[cfg(feature = "migrate")]
+pub use migrate::{migrate, MigrateArgs};
+
+#[cfg(feature = "watch")]
+mod watch;
+#[cfg(feature = "watch")]
+pub use watch::{watch, WatchArgs};
